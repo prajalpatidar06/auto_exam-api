9293f0ea045a266a7b559b445e53d7399fa85ae3
Prajal Patidar <prajalpatidar01@gmail.com>
2022-08-21 18:13:49 +0530

    all good

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+  res.send("All Good!");
+});
 router.use("/invigilators", require("./invigilators"));
-router.use("/evaluators",require('./evaluators'))
+router.use("/evaluators", require("./evaluators"));
 
 module.exports = router;
