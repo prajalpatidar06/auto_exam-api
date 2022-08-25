125f88c92a565fa86874af59fb7d4dca38494a7e
Prajal Patidar <prajalpatidar01@gmail.com>
2022-08-25 18:37:04 +0530

    fix typo in iv

--- models/sheets.js
+++ models/sheets.js
@@ -5,7 +5,7 @@ const SheetsSchema = mongoose.Schema({
   marks: { type: Number, default: 0 },
   subjectCode: { type: String },
   type: { type: String, default: "main", enum: ["main", "extra"] },
-  extras: [{ type: String, unique: true }],
+  extras: [{ type: String }],
   student: { type: mongoose.Schema.Types.ObjectId, ref: "Student" },
   evaluator: { type: mongoose.Schema.Types.ObjectId, ref: "Evaluator" },
   testPaperSet: { type: String },
